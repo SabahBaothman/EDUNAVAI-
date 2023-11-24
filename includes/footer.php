249e6001d9f86fd38adca253e0166e8767441288
Sabah <129803023+SabahBaothman@users.noreply.github.com>
2023-11-24 16:48:47 +0300

    Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div>
+        <h2>EDU NAV AI</h2>
+        <h5>Start your learning journey with your Education Navigator using AI.</h5>
+    </div>
+    
+    <nav>
+
+<!-- Holder for navigation links -->
+<span id="nav-links">
+    <a href="../pages/index.php" class="nav-link <?php echo ($currentPage === 'home') ? 'selected' : ''; ?>">Home</a>
+    <a href="../pages/Roadmap Generator.php" class="nav-link <?php echo ($currentPage === 'roadmap') ? 'selected' : ''; ?>">Roadmap Generator</a>
+    <a href="../pages/About us.php" class="nav-link <?php echo ($currentPage === 'about') ? 'selected' : ''; ?>">About Us</a>
+    <a href="../pages/Contact.php" class="nav-link <?php echo ($currentPage === 'contact') ? 'selected' : ''; ?>">Contact</a>
+    <a href="../pages/Table.php" class="nav-link <?php echo ($currentPage === 'Table') ? 'selected' : ''; ?>">Table</a>
+    <a href="../pages/PictureGallery.php" class="nav-link <?php echo ($currentPage === 'Pictur Gallery') ? 'selected' : ''; ?>">Pictur Gallery</a>
+    <a href="../pages/Feedback.php" class="nav-link <?php echo ($currentPage === 'Feedback') ? 'selected' : ''; ?>">Fedback</a>
+
+</span>
+
+</nav>
+
+    <hr>
+    <p>&copy; 2023 EduNavAi All rights reserved.</p>
+    <p>Jeddah EduNavAi@gmail.com</p>
+</footer>
