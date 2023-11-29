673fcf395eeccfa4810e2cc3517a2e766929e891
RIMA_T <84948128+rimagh19@users.noreply.github.com>
2023-11-29 19:59:53 +0300

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,19 +1,46 @@
 const button = document.getElementById('sendMsssage');
 
 button.addEventListener('click', (event) => {
-  const emailInput = document.getElementById('mail');
-  const characters = emailInput.value.split('');
+  event.preventDefault(); // Prevent the default form submission
+  const emailInput = document.getElementById('mail').value;
 
-  for (let i = 0; i < characters.length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    const tempChar = characters[i];
-    characters[i] = characters[randomIndex];
-    characters[randomIndex] = tempChar;
+  if (!verifyEmail(emailInput)) {
+    alert("Use a valid email syntax such as mail@provider.com");
+  } else {
+    submitForm();
+    
   }
-
-
-  const scrambledEmail = characters.join('');
-  emailInput.value = scrambledEmail;
-
-  alert("The email has been scrambled, the scrambled email is: "+emailInput.value); 
 });
+
+async function submitForm() {
+  const form = document.getElementById('contact_form');
+  const formData = new FormData(form);
+
+  fetch('../db/email.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Assuming the response is in JSON format
+  })
+  .then(data => {
+    console.log(data.message); // Handle the response from the server
+    alert(data.message); // Show an alert with the message
+    window.open("../pages/Contact.php", "_self"); // Redirect to another page
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+  
+
+}
+
+function verifyEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+
