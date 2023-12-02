bc632c70fc52e9c0417daeb7d44af80dee984d89
RIMA_T <84948128+rimagh19@users.noreply.github.com>
2023-12-02 15:00:37 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,50 @@
 <footer>
-    <div>
-        <h2>EDU NAV AI</h2>
-        <h5>Start your learning journey with your Education Navigator using AI.</h5>
+    <div id="footer-container">
+        <div class="left-half">
+            <span id="nav-logo-holder">
+                <img src="../media/images/logo-text.png" width="150px">
+            </span>
+            <h5>Start your learning journey with your Education Navigator using AI.</h5>
+        </div>
+        <div class="right-half footer-grid-wrapper ">
+            <div class="footer-block">
+                <div class="footer-title-text">Main</div>
+                <a href="./" aria-current="page" class="footer-text w--current">Home</a>
+                <a href="./About us.php" class="footer-text">About</a>
+            </div>
+
+            <div class="footer-block footer-grid-wrapper">
+                <div class="footer-title-text">Roadmap</div>
+                <a href="./Roadmap Generator.php" class="footer-text">Roadmap Generator</a>
+                <a href="./video.php" class="footer-text">Turotria</a>
+                <a href="./feedback.php" class="footer-text">Feedback</a>
+            </div>
+
+            <div class="footer-block">
+                <div class="footer-title-text">More Pages</div>
+                <a href="./table.php" class="footer-text">Frequently Asked Questions</a>
+                <a href="./PictureGallery.php" class="footer-text">Inspirational Quotes</a>
+                <a href="./contact.php" class="footer-text">Contact</a>
+
+            </div>
+
+            <div class="footer-block">
+                <div class="footer-title-text">Admin Pages</div>
+                <a href="./Dashboard.php" class="footer-text">Dashboard</a>
+            </div>
+        </div>
     </div>
-    
-    <nav>
+    <div id="footer-container">
+    <div class="h-line">
+                    <hr>
+        </div>
+        
 
-<!-- Holder for navigation links -->
-<span id="nav-links">
-    <a href="../pages/index.php" class="nav-link <?php echo ($currentPage === 'home') ? 'selected' : ''; ?>">Home</a>
-    <a href="../pages/Roadmap Generator.php" class="nav-link <?php echo ($currentPage === 'roadmap') ? 'selected' : ''; ?>">Roadmap Generator</a>
-    <a href="../pages/About us.php" class="nav-link <?php echo ($currentPage === 'about') ? 'selected' : ''; ?>">About Us</a>
-    <a href="../pages/Contact.php" class="nav-link <?php echo ($currentPage === 'contact') ? 'selected' : ''; ?>">Contact</a>
-    <a href="../pages/Table.php" class="nav-link <?php echo ($currentPage === 'Table') ? 'selected' : ''; ?>">Table</a>
-    <a href="../pages/PictureGallery.php" class="nav-link <?php echo ($currentPage === 'Pictur Gallery') ? 'selected' : ''; ?>">Pictur Gallery</a>
-    <a href="../pages/Feedback.php" class="nav-link <?php echo ($currentPage === 'Feedback') ? 'selected' : ''; ?>">Fedback</a>
-
-</span>
-
-</nav>
-
-    <hr>
-    <p>&copy; 2023 EduNavAi All rights reserved.</p>
-    <p>Jeddah edunavigationai@gmail.com</p>
+    </div>
+    <div id="footer-container">
+        <p>&copy; 2023 EduNavAi All rights reserved.</p>
+        </div>
+        <div id="footer-container">
+        <p>Jeddah edunavigationai@gmail.com</p>
+        </div>
 </footer>
