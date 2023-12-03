f934508192bb855bb9337b085fc438c4666cd27c
RIMA_T <84948128+rimagh19@users.noreply.github.com>
2023-12-03 16:54:14 +0300

    Update links.php

--- includes/links.php
+++ includes/links.php
@@ -3,6 +3,7 @@
 <nav>
 
 <!-- Holder for the website logo -->
+<a href="../pages/index.php" style="display: flex;">
 <span id="nav-logo-holder">
     <img id="logo" src="../media/images/logo.png" width="80px">
 </span>
@@ -10,14 +11,13 @@
 <span id="nav-title-holder">
     <h3><?php echo $pageTitle; ?></h3>
 </span>
-
+</a>
 <!-- Holder for navigation links -->
 <span id="nav-links">
-    <a href="../pages/index.php" class="nav-link <?php echo ($currentPage === 'home') ? 'selected' : ''; ?>">Home</a>
     <a href="../pages/Roadmap Generator.php" class="nav-link <?php echo ($currentPage === 'roadmap') ? 'selected' : ''; ?>">Roadmap Generator</a>
     <a href="../pages/About us.php" class="nav-link <?php echo ($currentPage === 'about') ? 'selected' : ''; ?>">About Us</a>
     <a href="../pages/Contact.php" class="nav-link <?php echo ($currentPage === 'contact') ? 'selected' : ''; ?>">Contact</a>
-    <a href="../pages/Table.php" class="nav-link <?php echo ($currentPage === 'Table') ? 'selected' : ''; ?>">Table</a>
+    <a href="../pages/FAQ.php" class="nav-link <?php echo ($currentPage === 'Table') ? 'selected' : ''; ?>">FAQ</a>
     <a href="../pages/PictureGallery.php" class="nav-link <?php echo ($currentPage === 'Pictur Gallery') ? 'selected' : ''; ?>">Pictur Gallery</a>
     <a href="../pages/Feedback.php" class="nav-link <?php echo ($currentPage === 'Feedback') ? 'selected' : ''; ?>">Fedback</a>
 
